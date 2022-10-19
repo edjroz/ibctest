75c7eb97e89ea4cf9116a10e540752f9401793a0
Radu Gheorghies <radu.gheorghies@gmail.com>
2022-10-19 17:57:37 +0300

    adding the light client - not working yet

--- examples/polkadot/polkadot_chain_test.go
+++ examples/polkadot/polkadot_chain_test.go
@@ -3,12 +3,23 @@ package polkadot_test
 import (
 	"context"
 	"testing"
+	"os"
 
 	"github.com/strangelove-ventures/ibctest/v6"
 	"github.com/strangelove-ventures/ibctest/v6/ibc"
 	"github.com/strangelove-ventures/ibctest/v6/test"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
+	lightclient "github.com/ComposableFi/go-substrate-rpc-client/v4"
+	"github.com/strangelove-ventures/ibctest/v6/testreporter"
+	// rpcclienttypes "github.com/ComposableFi/go-substrate-rpc-client/v4/types"
+	// beefytypes "github.com/ComposableFi/ics11-beefy/types"
+)
+
+var (
+	BEEFY_TEST_MODE    = os.Getenv("BEEFY_TEST_MODE")
+	RPC_CLIENT_ADDRESS = os.Getenv("RPC_CLIENT_ADDRESS")
+	UPDATE_STATE_MODE  = os.Getenv("UPDATE_STATE_MODE")
 )
 
 func TestPolkadotComposableChainStart(t *testing.T) {
@@ -33,12 +44,16 @@ func TestPolkadotComposableChainStart(t *testing.T) {
 			NumValidators: &nv,
 			NumFullNodes:  &nf,
 		},
+		{Name: "gaia", Version: "v7.0.0", ChainConfig: ibc.ChainConfig{
+			GasPrices: "0.0uatom",
+		}},
 	},
 	).Chains(t.Name())
 
 	require.NoError(t, err, "failed to get polkadot chain")
-	require.Len(t, chains, 1)
+	require.Len(t, chains, 2)
 	chain := chains[0]
+	chain2 := chains[1]
 
 	ctx := context.Background()
 
@@ -50,4 +65,53 @@ func TestPolkadotComposableChainStart(t *testing.T) {
 
 	err = test.WaitForBlocks(ctx, 10, chain)
 	require.NoError(t, err, "polkadot chain failed to make blocks")
+
+	// beefy
+	t.Skip("skipping TestCheckHeaderAndUpdateState")
+	// if BEEFY_TEST_MODE != "true" {
+	// 	t.Skip("skipping test in short mode")
+	// }
+	if RPC_CLIENT_ADDRESS == "" {
+		t.Log("==== RPC_CLIENT_ADDRESS not set, will use default ==== ")
+		RPC_CLIENT_ADDRESS = "ws://127.0.0.1:9944"
+	}
+
+	relayApi, err := lightclient.NewSubstrateAPI(RPC_CLIENT_ADDRESS)
+	require.NoError(t, err)
+
+	t.Log("==== connected! ==== ", relayApi)
+
+	rep := testreporter.NewNopReporter()
+	dClient, network := ibctest.DockerSetup(t)
+
+	// Get a relayer instance
+	r := ibctest.NewBuiltinRelayerFactory(
+		ibc.CosmosRly,
+		zaptest.NewLogger(t),
+		// relayApi.RelayerOptionExtraStartFlags{Flags: []string{"-p", "events", "-b", "100"}},
+	).Build(t, dClient, network)
+
+	// Build the network; spin up the chains and configure the relayer
+	const pathName = "test-path"
+	const relayerName = "relayer"
+	eRep := rep.RelayerExecReporter(t)
+
+
+	ic := ibctest.NewInterchain().
+		AddChain(chain).
+		AddChain(chain2).
+		AddRelayer(r, relayerName).
+		AddLink(ibctest.InterchainLink{
+			Chain1:  chain,
+			Chain2:  chain2,
+			Relayer: r,
+			Path:    pathName,
+		})
+
+	require.NoError(t, ic.Build(ctx, eRep, ibctest.InterchainBuildOptions{
+		TestName:         t.Name(),
+		Client:           client,
+		NetworkID:        network,
+		SkipPathCreation: true,
+	}))
 }
