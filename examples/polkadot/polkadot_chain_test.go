c6b2442e94c48538bfa41951fd4b25921e8c5902
Radu Gheorghies <radu.gheorghies@gmail.com>
2022-10-21 11:35:51 +0300

    light client non working version

--- examples/polkadot/polkadot_chain_test.go
+++ examples/polkadot/polkadot_chain_test.go
@@ -52,25 +52,23 @@ func TestPolkadotComposableChainStart(t *testing.T) {
 
 	require.NoError(t, err, "failed to get polkadot chain")
 	require.Len(t, chains, 2)
-	chain := chains[0]
+	chain1 := chains[0]
 	chain2 := chains[1]
 
 	ctx := context.Background()
 
-	err = chain.Initialize(ctx, t.Name(), client, network)
+	err = chain1.Initialize(ctx, t.Name(), client, network)
 	require.NoError(t, err, "failed to initialize polkadot chain")
 
-	err = chain.Start(t.Name(), ctx)
+	err = chain1.Start(t.Name(), ctx)
 	require.NoError(t, err, "failed to start polkadot chain")
 
-	err = test.WaitForBlocks(ctx, 10, chain)
+	err = test.WaitForBlocks(ctx, 10, chain1)
 	require.NoError(t, err, "polkadot chain failed to make blocks")
 
 	// beefy
 	t.Skip("skipping TestCheckHeaderAndUpdateState")
-	// if BEEFY_TEST_MODE != "true" {
-	// 	t.Skip("skipping test in short mode")
-	// }
+	
 	if RPC_CLIENT_ADDRESS == "" {
 		t.Log("==== RPC_CLIENT_ADDRESS not set, will use default ==== ")
 		RPC_CLIENT_ADDRESS = "ws://127.0.0.1:9944"
@@ -82,14 +80,26 @@ func TestPolkadotComposableChainStart(t *testing.T) {
 	t.Log("==== connected! ==== ", relayApi)
 
 	rep := testreporter.NewNopReporter()
-	dClient, network := ibctest.DockerSetup(t)
+	// dClient, network := ibctest.DockerSetup(t)
 
+	ch := make(chan interface{})
 	// Get a relayer instance
+	_, err = relayApi.Client.Subscribe(
+		context.Background(),
+		"beefy",
+		"subscribeJustifications",
+		"unsubscribeJustifications",
+		"justifications",
+		ch,
+	)
+	require.NoError(t, err)
+
+
 	r := ibctest.NewBuiltinRelayerFactory(
 		ibc.CosmosRly,
 		zaptest.NewLogger(t),
 		// relayApi.RelayerOptionExtraStartFlags{Flags: []string{"-p", "events", "-b", "100"}},
-	).Build(t, dClient, network)
+	).Build(t, relayApi., network)
 
 	// Build the network; spin up the chains and configure the relayer
 	const pathName = "test-path"
@@ -97,11 +107,11 @@ func TestPolkadotComposableChainStart(t *testing.T) {
 	eRep := rep.RelayerExecReporter(t)
 
 	ic := ibctest.NewInterchain().
-		AddChain(chain).
+		AddChain(chain1).
 		AddChain(chain2).
 		AddRelayer(r, relayerName).
 		AddLink(ibctest.InterchainLink{
-			Chain1:  chain,
+			Chain1:  chain1,
 			Chain2:  chain2,
 			Relayer: r,
 			Path:    pathName,
@@ -113,4 +123,85 @@ func TestPolkadotComposableChainStart(t *testing.T) {
 		NetworkID:        network,
 		SkipPathCreation: true,
 	}))
+
+
+
+
+	if testing.Short() {
+		t.Skip()
+	}
+
+	t.Parallel()
+
+	client, network := ibctest.DockerSetup(t)
+
+	nv := 5
+	nf := 3
+
+	rep := testreporter.NewNopReporter()
+	eRep := rep.RelayerExecReporter(t)
+
+	ctx := context.Background()
+
+	// Get both chains
+	cf := ibctest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*ibctest.ChainSpec{
+		{
+			Name:    "composable",
+			Version: "polkadot:v0.9.19,composable:centauri",
+			ChainConfig: ibc.ChainConfig{
+				ChainID: "rococo-local",
+			},
+			NumValidators: &nv,
+			NumFullNodes:  &nf,
+		},
+		{
+			Name:    "composable",
+			Version: "polkadot:v0.9.19,composable:centauri",
+			ChainConfig: ibc.ChainConfig{
+				ChainID: "rococo-local",
+			},
+			NumValidators: &nv,
+			NumFullNodes:  &nf,
+		},
+	})
+
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	chain1, chain2 := chains[0], chains[1]
+
+	// Get a relayer instance
+	r := ibctest.NewBuiltinRelayerFactory(
+		ibc.CosmosRly,
+		zaptest.NewLogger(t),
+		relayer.RelayerOptionExtraStartFlags{Flags: []string{"-p", "events", "-b", "100"}},
+	).Build(t, client, network)
+
+	// Build the network; spin up the chains and configure the relayer
+	const pathName = "test-path"
+	const relayerName = "relayer"
+
+	ic := ibctest.NewInterchain().
+		AddChain(chain1).
+		AddChain(chain2).
+		AddRelayer(r, relayerName).
+		AddLink(ibctest.InterchainLink{
+			Chain1:  chain1,
+			Chain2:  chain2,
+			Relayer: r,
+			Path:    pathName,
+		})
+
+	require.NoError(t, ic.Build(ctx, eRep, ibctest.InterchainBuildOptions{
+		TestName:         t.Name(),
+		Client:           client,
+		NetworkID:        network,
+		SkipPathCreation: true,
+	}))
+
+	// Fund a user account on chain1 and chain2
+	const userFunds = int64(10_000_000_000)
+	users := ibctest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, chain1, chain2)
+	// chain1User := users[0]
+	// chain2User := users[1]
 }
