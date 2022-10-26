5b6f4b04d66fedf71f7097623c9412a54792395b
Osho Emmanuel <oshoklinsmann@gmail.com>
2022-10-26 15:10:26 +0100

    add link test for polkadot <> cosmos chain

--- examples/polkadot/polkadot_chain_test.go
+++ examples/polkadot/polkadot_chain_test.go
@@ -2,6 +2,8 @@ package polkadot_test
 
 import (
 	"context"
+	"github.com/strangelove-ventures/ibctest/v6/relayer"
+	"github.com/strangelove-ventures/ibctest/v6/testreporter"
 	"testing"
 
 	"github.com/strangelove-ventures/ibctest/v6"
@@ -50,4 +52,85 @@ func TestPolkadotComposableChainStart(t *testing.T) {
 
 	err = test.WaitForBlocks(ctx, 10, chain)
 	require.NoError(t, err, "polkadot chain failed to make blocks")
+}
+
+func TestCreatePolakdotCosmosLink(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	t.Parallel()
+
+	rep := testreporter.NewNopReporter()
+	eRep := rep.RelayerExecReporter(t)
+
+	client, network := ibctest.DockerSetup(t)
+
+	nv := 5
+	nf := 3
+
+	chains, err := ibctest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*ibctest.ChainSpec{
+		{
+			Name:    "composable",
+			Version: "polkadot:v0.9.19,composable:centauri",
+			ChainConfig: ibc.ChainConfig{
+				ChainID: "rococo-local",
+			},
+			NumValidators: &nv,
+			NumFullNodes:  &nf,
+		},
+		{
+			Name:      "gaia",
+			ChainName: "gaia",
+			Version:   "v7.0.3",
+		},
+	},
+	).Chains(t.Name())
+
+	require.NoError(t, err, "failed to get polkadot chain")
+	require.Len(t, chains, 2)
+	chain1 := chains[0]
+	chain2 := chains[1]
+
+	ctx := context.Background()
+
+	err = chain1.Initialize(ctx, t.Name(), client, network)
+	require.NoError(t, err, "failed to initialize polkadot chain")
+
+	err = chain1.Start(t.Name(), ctx)
+	require.NoError(t, err, "failed to start polkadot chain")
+
+	err = test.WaitForBlocks(ctx, 10, chain1)
+	require.NoError(t, err, "polkadot chain failed to make blocks")
+
+	err = chain2.Initialize(ctx, t.Name(), client, network)
+	require.NoError(t, err, "failed to initialize gaia chain")
+
+	err = chain2.Start(t.Name(), ctx)
+	require.NoError(t, err, "failed to start gaia chain")
+
+	err = test.WaitForBlocks(ctx, 10, chain2)
+	require.NoError(t, err, "gaia chain failed to make blocks")
+
+	r := ibctest.NewBuiltinRelayerFactory(ibc.CosmosRly, zaptest.NewLogger(t), relayer.ImagePull(false),
+		relayer.CustomDockerImage("relayer", "feat-substrate", "100:1000")).Build(
+		t, client, network,
+	)
+
+	ic := ibctest.NewInterchain().
+		AddChain(chain1).
+		AddChain(chain2).
+		AddRelayer(r, "r").
+		AddLink(ibctest.InterchainLink{
+			Chain1:  chain1,
+			Chain2:  chain2,
+			Relayer: r,
+		})
+
+	require.NoError(t, ic.Build(ctx, eRep, ibctest.InterchainBuildOptions{
+		TestName:         t.Name(),
+		Client:           client,
+		NetworkID:        network,
+		SkipPathCreation: false,
+	}))
 }
